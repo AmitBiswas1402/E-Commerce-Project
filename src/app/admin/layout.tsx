99a0a09b4fd2e58f2bc00ac6abc3d39456541688
Amit Biswas <amit.142biswas@gmail.com>
2024-07-08 17:12:55 +0530

    dvh

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,3 +1,4 @@
+import Nav, { NavLink } from '@/components/Nav';
 import React from 'react'
 
 export default function layout({
@@ -8,6 +9,10 @@ export default function layout({
   return (
     <>
         <Nav>
+          <NavLink href='/admin'>Dashboard</NavLink>
+          <NavLink href='/admin/products'>Products</NavLink>
+          <NavLink href='/admin/users'>Customer</NavLink>
+          <NavLink href='/admin/orders'>Orders</NavLink>
             
         </Nav>
         <div className='container my-6'>{children}</div>
---
+++ src/app/admin/page.tsx
@@ -0,0 +1,32 @@
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+
+
+export default function AdmindDashBoard() {
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            <DashboardCard title="sales" subtitle="" body="body" />
+            
+            
+        </div>
+    )
+}
+
+type DashboardCardProps = {
+    title: string,
+    subtitle: string,
+    body: string
+}
+
+function DashboardCard({title, subtitle, body}: DashboardCardProps){
+    return (
+        <Card>
+            <CardHeader>
+                <CardTitle>{title}</CardTitle>
+                <CardDescription>{subtitle}</CardDescription>
+            </CardHeader>
+            <CardContent>
+                <p>{body}</p>
+            </CardContent>
+        </Card>
+    )
+}
